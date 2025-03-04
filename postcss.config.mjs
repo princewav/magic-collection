6668c24123fe720b4998e665efd4c23826fcf6cd
princewav <principe.edoardo@outlook.it>
2025-03-04 18:02:44 +0100

    refactor: Configure tailwindcss as a nested object in postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,10 @@
 import path from 'path';
-import tailwindcss from '@tailwindcss/postcss';
 
 export default {
   plugins: {
-    tailwindcss: tailwindcss({ 
+    tailwindcss: { 
       config: path.resolve('./tailwind.config.js') 
-    }),
+    },
     autoprefixer: {},
   },
 }
