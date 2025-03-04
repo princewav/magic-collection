18892a4921632abd2cc94f4112e31d0cd9c3c240
princewav <principe.edoardo@outlook.it>
2025-03-04 09:06:00 +0100

    feat: Update ManaSymbol component to include multicolor symbol

--- src/app/components/Filters.tsx
+++ src/app/components/Filters.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import ManaSymbol from "./ManaSymbol";
 
 export default function Filters() {
--- src/app/components/ManaSymbol.tsx
+++ src/app/components/ManaSymbol.tsx
@@ -26,35 +26,8 @@ export default function ManaSymbol({ symbol }: ManaSymbolProps) {
     case "{C}":
       imageSrc = "/images/mana/colorless.svg";
       break;
-    case "{W/U}":
-      imageSrc = "/images/mana/wu.svg";
-      break;
-    case "{W/B}":
-      imageSrc = "/images/mana/wb.svg";
-      break;
-    case "{U/B}":
-      imageSrc = "/images/mana/ub.svg";
-      break;
-    case "{U/R}":
-      imageSrc = "/images/mana/ur.svg";
-      break;
-    case "{B/R}":
-      imageSrc = "/images/mana/br.svg";
-      break;
-    case "{B/G}":
-      imageSrc = "/images/mana/bg.svg";
-      break;
-    case "{R/G}":
-      imageSrc = "/images/mana/rg.svg";
-      break;
-    case "{R/W}":
-      imageSrc = "/images/mana/rw.svg";
-      break;
-    case "{G/W}":
-      imageSrc = "/images/mana/gw.svg";
-      break;
-    case "{G/U}":
-      imageSrc = "/images/mana/gu.svg";
+    case "{M}":
+      imageSrc = "/images/mana/multicolor.svg";
       break;
     default:
       imageSrc = "/images/mana/unknown.svg"; // Provide a default image
