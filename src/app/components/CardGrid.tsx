5649b66a2260721300128b332d3ef7db382bbdf3
princewav <principe.edoardo@outlook.it>
2025-03-04 18:09:53 +0100

    fix: Update card IDs in CardGrid component

--- src/app/components/CardGrid.tsx
+++ src/app/components/CardGrid.tsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 export default function CardGrid() {
   return (
     <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
-      <Card id="038a7ee1-5352-4562-9e51-38ca7e9c60d4" /> {/* Example ID: Sol Ring */}
+      <Card id="c6399a22-cebf-4c1d-a23e-4c68f784ac1b" /> {/* Example ID: Sol Ring */}
       <Card id="bc728618-0aa9-40d5-829a-47da9b743ce3" /> {/* Example ID: Lightning Bolt */}
       <Card id="6cbbca60-6a4a-45eb-8266-3e4248bf130d" /> {/* Example ID: Counterspell */}
       <Card id="59df2f8f-5dcd-4f5b-ba72-36e677f9406b" /> {/* Example ID: Black Lotus */}
