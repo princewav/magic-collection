f61bb1160b40aa210d49ab20cad447d958cc2b4e
princewav <principe.edoardo@outlook.it>
2025-03-06 17:11:33 +0100

    feat: add style for checked decks

--- src/components/deck/Deck.tsx
+++ src/components/deck/Deck.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Checkbox } from '@/components/ui/checkbox';
 
 import { ManaSymbol } from '@/components/ManaSymbol';
+import { cn } from '@/lib/utils';
 
 interface DeckProps {
   deck: {
@@ -26,6 +27,8 @@ export const Deck: React.FC<DeckProps> = ({
   onCheck,
   isChecked,
 }) => {
+  const [localChecked, setLocalChecked] = useState(isChecked);
+
   const handleContextMenu = useCallback(
     (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
       e.preventDefault();
@@ -36,28 +39,36 @@ export const Deck: React.FC<DeckProps> = ({
   );
 
   const handleCheckboxChange = useCallback(() => {
+    const newCheckState = !localChecked;
+    setLocalChecked(newCheckState);
     onCheck(deck.id);
-  }, [deck.id, onCheck]);
+    console.log(deck.id, newCheckState);
+  }, [deck.id, localChecked]);
 
   return (
     <div
       key={deck.id}
-      className="bg-card hover:bg-card/90 border-muted relative w-70 overflow-hidden rounded-lg border shadow-md transition-all duration-200 hover:scale-105 hover:shadow-lg"
+      className={cn(
+        'group bg-card hover:bg-card/90 border-muted relative w-70 overflow-hidden rounded-lg border shadow-md transition-all duration-200 hover:scale-105 hover:shadow-lg',
+        localChecked
+          ? 'bg-foreground/10 hover:bg-foreground/20 scale-[1.06] hover:scale-[1.07]'
+          : '',
+      )}
     >
       <Checkbox
         id={`deck-${deck.id}`}
-        className="border-primary/50 text-primary focus:border-primary focus:ring-primary absolute top-2 left-2 z-10 size-5 cursor-pointer rounded-full border-3 shadow-sm"
-        checked={isChecked}
+        className="border-primary/50 text-primary focus:border-primary focus:ring-primary absolute top-2 left-2 z-10 size-7 cursor-pointer rounded-full border-3 shadow-sm"
+        checked={localChecked}
         onCheckedChange={handleCheckboxChange}
       />
       <Link href={`/decks/${deck.id}`} onContextMenu={handleContextMenu}>
-        <div className="relative h-48">
+        <div className="relative h-48 overflow-hidden">
           {deck.imageUrl ? (
             <Image
               src={deck.imageUrl}
               alt={deck.name}
               fill
-              className="object-cover"
+              className="object-cover transition-all duration-300 ease-in-out group-hover:scale-[1.06]"
             />
           ) : (
             <div className="bg-muted flex h-full w-full items-center justify-center">
