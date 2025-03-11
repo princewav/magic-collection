48125945a5ff4ea5fa4d0586125bbac2602d441b
princewav <principe.edoardo@outlook.it>
2025-03-11 04:36:47 +0100

    refactor: moved navbar component to its own folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next/types';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { CardModalProvider } from '@/context/CardModalContext';
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/navbar/Navbar';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/sonner';
 
