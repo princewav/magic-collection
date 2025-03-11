c8089f1f5ca1e5719f780980b85f56039113498d
princewav <principe.edoardo@outlook.it>
2025-03-11 05:16:23 +0100

    feat: improved style of navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,22 +18,114 @@ import './Navbar.css';
 
 interface NavbarProps {}
 
+interface Props extends React.HTMLAttributes<HTMLElement> {
+  title: string;
+  children?: React.ReactNode;
+  href: string;
+}
+
+const NavItem = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <NavigationMenuLink asChild>
+      <Link
+        href={href}
+        className={cn(
+          'bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50',
+        )}
+      >
+        {children}
+      </Link>
+    </NavigationMenuLink>
+  );
+};
+
+const NavDropdown = ({
+  triggerContent,
+  children,
+}: {
+  triggerContent: React.ReactNode;
+  children: React.ReactNode;
+}) => {
+  return (
+    <NavigationMenuItem>
+      <NavigationMenuTrigger className="group">
+        {triggerContent}
+      </NavigationMenuTrigger>
+      <NavigationMenuContent className="NavigationMenuContent absolute top-0 left-0 w-full">
+        {children}
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  );
+};
+
+const DropdownList = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+      {children}
+    </ul>
+  );
+};
+
+const DropdownOverview = ({
+  title,
+  description,
+  href,
+  imageSrc,
+}: {
+  title: string;
+  description: string;
+  href: string;
+  imageSrc?: string;
+}) => {
+  return (
+    <li className="row-span-2">
+      <NavigationMenuLink asChild>
+        <a
+          className="from-muted/50 to-muted flex size-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none select-none focus:shadow-md"
+          href={href}
+        >
+          {imageSrc && (
+            <Image src={imageSrc} alt="" width={15} height={15} />
+          )}
+          <div className="mb-2 text-lg font-medium">{title}</div>
+          <p className="text-muted-foreground text-sm leading-tight">
+            {description}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+};
+
 const ListItem = React.forwardRef<
   React.ComponentRef<'a'>,
-  React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<'a'> & {
+    title: string;
+    imageSrc: string;
+    children?: React.ReactNode;
+  }
+> (({ className, title, imageSrc, children, ...props }, ref) => (
   <li>
     <NavigationMenuLink asChild>
       <a
         className={cn(
-          'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-          className
+          'hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block space-y-1 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none',
+          className,
         )}
         ref={ref}
         {...props}
       >
-        <div className='text-sm font-medium leading-none'>{title}</div>
-        <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+        <div className="flex flex-row items-center space-x-1">
+          <Image src={imageSrc} alt="" width={15} height={15} />
+          <div className="text-sm leading-none font-medium">{title}</div>
+        </div>
+        <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
           {children}
         </p>
       </a>
@@ -44,105 +136,83 @@ ListItem.displayName = 'ListItem';
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
-    <nav className="p-4 shadow-md">
+    <nav className="px-4 py-2 shadow-md">
       <div className="container flex items-center">
         <div className="mr-4 text-2xl font-bold">Magic Collection</div>
         <NavigationMenu>
-          <NavigationMenuList className='relative'>
+          <NavigationMenuList className="relative">
             {/* Browse */}
             <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link
-                  href="/"
-                  className={cn(
-                    'inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50'
-                  )}
-                >
-                  <Home className="h-5 w-5" />
+              <NavItem href="/">
+                <div className="flex flex-row items-center space-x-2">
+                  <Home className="!h-5 !w-5" color="white" />
                   <span>Browse</span>
-                </Link>
-              </NavigationMenuLink>
+                </div>
+              </NavItem>
             </NavigationMenuItem>
 
             {/* Decks Menu */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className='group'>
-                <Book className="h-5 w-5" />
-                <span>Decks</span>
-              </NavigationMenuTrigger>
-              <NavigationMenuContent className='NavigationMenuContent absolute left-0 top-0 w-full'>
-                <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-                  <li className='row-span-2'>
-                    <NavigationMenuLink asChild>
-                      <a
-                        className='flex size-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
-                        href='/decks'
-                      >
-                        <div className='mb-2 text-lg font-medium'>Decks</div>
-                        <p className='text-sm leading-tight text-muted-foreground'>
-                          Manage your Magic: The Gathering decks across formats
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href='/decks/paper' title='Paper Decks'>
-                    Manage your physical Magic: The Gathering decks
-                  </ListItem>
-                  <ListItem href='/decks/arena' title='Arena Decks'>
-                    Organize and track your MTG Arena decks
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+            <NavDropdown
+              triggerContent={
+                <div className="flex flex-row space-x-2">
+                  <Book className="h-5 w-5" />
+                  <span>Decks</span>
+                </div>
+              }
+            >
+              <DropdownList>
+                <DropdownOverview
+                  title="Decks"
+                  description="Manage your Magic: The Gathering decks across formats"
+                  href="/decks"
+                  imageSrc="/images/deck-w.png"
+                />
+                <ListItem href="/decks/paper" title="Paper Decks" imageSrc="/images/card-w.png">
+                  Manage your physical Magic: The Gathering decks
+                </ListItem>
+                <ListItem href="/decks/arena" title="Arena Decks" imageSrc="/images/arena-w.png">
+                  Organize and track your MTG Arena decks
+                </ListItem>
+              </DropdownList>
+            </NavDropdown>
 
             {/* Collection Menu */}
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className='group'>
-                <ListChecks className="h-5 w-5" />
-                <span>Collection</span>
-              </NavigationMenuTrigger>
-              <NavigationMenuContent className='NavigationMenuContent absolute left-0 top-0 w-full'>
-                <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-                  <li className='row-span-2'>
-                    <NavigationMenuLink asChild>
-                      <a
-                        className='flex size-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
-                        href='/collection'
-                      >
-                        <div className='mb-2 text-lg font-medium'>Collection</div>
-                        <p className='text-sm leading-tight text-muted-foreground'>
-                          Track your Magic cards across platforms
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href='/collection/paper' title='Paper Collection'>
-                    Catalog your physical Magic cards
-                  </ListItem>
-                  <ListItem href='/collection/arena' title='Arena Collection'>
-                    Track your MTG Arena card collection
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+            <NavDropdown
+              triggerContent={
+                <div className="flex flex-row space-x-2">
+                  <ListChecks className="h-5 w-5" />
+                  <span>Collection</span>
+                </div>
+              }
+            >
+              <DropdownList>
+                <DropdownOverview
+                  title="Collection"
+                  description="Track your Magic cards across platforms"
+                  href="/collection"
+                  imageSrc="/images/card-w.png"
+                />
+                <ListItem href="/collection/paper" title="Paper Collection" imageSrc="/images/card-w.png">
+                  Catalog your physical Magic cards
+                </ListItem>
+                <ListItem href="/collection/arena" title="Arena Collection" imageSrc="/images/arena-w.png">
+                  Track your MTG Arena card collection
+                </ListItem>
+              </DropdownList>
+            </NavDropdown>
 
             {/* Wishlist */}
             <NavigationMenuItem>
-              <NavigationMenuLink asChild>
-                <Link
-                  href="/wishlists"
-                  className={cn(
-                    'inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50'
-                  )}
-                >
-                  <Heart className="h-5 w-5" />
+              <NavItem href="/wishlists">
+                <div className="flex flex-row items-center space-x-2">
+                  <Heart className="!h-5 !w-5" color="white" />
                   <span>Wishlists</span>
-                </Link>
-              </NavigationMenuLink>
+                </div>
+              </NavItem>
             </NavigationMenuItem>
           </NavigationMenuList>
 
-          <NavigationMenuViewport className='NavigationMenuViewport' />
+          <NavigationMenuViewport className="NavigationMenuViewport" />
         </NavigationMenu>
       </div>
     </nav>
