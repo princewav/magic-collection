9f27b557fc20e8b2f983ad7899374c4e5ce2ea6f
princewav <principe.edoardo@outlook.it>
2025-04-11 17:20:41 +0200

    refactor(wishlist): enhance WishlistInfo styling and layout

--- src/components/wishlist/WishlistInfo.tsx
+++ src/components/wishlist/WishlistInfo.tsx
@@ -33,17 +33,24 @@ export const WishlistInfo = ({ wishlist }: Props) => {
   const colors = getColors();
 
   return (
-    <div className="bg-foreground/10 mb-4 flex items-center justify-between rounded-md p-4 shadow-md">
+    <div className="bg-foreground/10 relative mb-4 flex items-center justify-between overflow-hidden rounded-md p-4 shadow-md min-w-[370px]">
+      <Image
+        src={wishlist.imageUrl || '/placeholder-wishlist.jpg'}
+        alt={wishlist.name}
+        width={1500}
+        height={1000}
+        className="absolute inset-0 -z-10 rounded-md object-cover object-center opacity-30 md:hidden"
+      />
       <div className="flex items-center space-x-4">
         <Image
           src={wishlist.imageUrl || '/placeholder-wishlist.jpg'}
           alt={wishlist.name}
           width={150}
           height={100}
-          className="rounded-md"
+          className="hidden rounded-md md:block"
         />
         <div>
-          <h2 className="text-2xl font-semibold">
+          <h2 className="drop-shadow-black text-2xl font-semibold drop-shadow-xl">
             {wishlist.name}{' '}
             <div className="mt-1 mb-2 flex items-center space-x-1">
               {Array.isArray(colors) && colors.length > 0
@@ -59,7 +66,7 @@ export const WishlistInfo = ({ wishlist }: Props) => {
       <div className="flex flex-col items-end justify-between space-y-2">
         <Link href={`/wishlists/${wishlist.id}/edit`}>
           <Button className="">
-            <Edit className="md:mr-2 h-4 w-4" />
+            <Edit className="h-4 w-4 md:mr-2" />
             <span className="hidden md:block">Edit Wishlist</span>
           </Button>
         </Link>
