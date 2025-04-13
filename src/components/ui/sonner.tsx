aba532814b09dd5013fda6ee8ef2718443bb5a31
Edoardo Principe <principe.edoardo@outlook.it>
2025-04-13 11:17:15 +0200

    refactor(sonner): standardize string quotes and improve toast options for enhanced readability and consistency

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,31 +1,32 @@
-"use client"
+'use client';
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { useTheme } from 'next-themes';
+import { Toaster as Sonner, ToasterProps } from 'sonner';
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = 'system' } = useTheme();
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme as ToasterProps['theme']}
       className="toaster group"
+      mobileOffset={64}
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
+            'group toast group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+          description: 'group-[.toast]:text-muted-foreground',
           actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground font-medium",
+            'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground font-medium',
           cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground font-medium",
+            'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground font-medium',
           success: '!bg-green-900/60 !text-white',
           error: '!bg-red-900/60 !text-white',
         },
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
