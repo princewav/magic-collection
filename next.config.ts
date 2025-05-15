76d89a92778342f328729b83747d6a0fa6117b05
princewav <principe.edoardo@outlook.it>
2025-05-15 18:42:35 +0200

    refactor(next-config): add error handling configuration for Scryfall

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/cards/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'errors.scryfall.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 
